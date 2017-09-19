9823d837963c829015204e15fc58b92dfa295a25
rodrigolima <rodrigolima@10.250.1.151>
2017-09-18 20:14:22 -0400

    initial upload

--- src/labweek2/rodrigolima/LabWeek2RodrigoLima.java
+++ src/labweek2/rodrigolima/LabWeek2RodrigoLima.java
@@ -5,6 +5,8 @@
  */
 package labweek2.rodrigolima;
 
+import java.util.Scanner;
+
 /**
  *
  * @author rodrigolima
@@ -14,9 +16,22 @@ public class LabWeek2RodrigoLima {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-     System.out.printf(" eu sou bom ");
-     
+    public static void main(String[] args) 
+    
+    {
+                System.out.print("Please enter your first favorite app: ");
+                Scanner keyboard = new Scanner(System.in);
+                String firstapp = keyboard.nextLine();
+                
+                System.out.print("Please enter your second favorite app: ");
+              
+                String secondapp = keyboard.nextLine();
+                
+                System.out.print("Please enter your third favorite app: ");
+                
+                String thirdapp = keyboard.nextLine();
+                
+                System.out.printf("Your favourite apps are: %s, %s and %s.%n", firstapp, secondapp, thirdapp);
     }
     
 }
